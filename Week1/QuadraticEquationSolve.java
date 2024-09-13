7a7e7da992d6d954faee0500258e94ec73d560de
S.Saha <soumabhasaha1509+github@gmail.com>
2024-09-14 00:59:27 +0530

    minor changes

--- Week1/QuadraticEquationSolve.java
+++ Week1/QuadraticEquationSolve.java
@@ -2,7 +2,7 @@
 public class QuadraticEquationSolve {
 
     public static void solve(double a, double b, double c) {
-        double discriminant = b * b - 4 * a * c;
+        double discriminant = ((b * b) - (4 * a * c));
         if (discriminant > 0) {
             double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
             double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
@@ -25,9 +25,9 @@ public class QuadraticEquationSolve {
     public static void main(String[] args) {
         System.out.println("Solving equation 5x^2 + 4x + 3 = 0");
         QuadraticEquationSolve.solve(5, 4, 3);
-        System.out.println("Solving equation x^2 + 4x + 10 = 0");
+        System.out.println("Solving equation x^2 + 4x + 4 = 0");
         QuadraticEquationSolve.solve(1, 4, 4);
-        System.out.println("Solving equation x^2 - 4x + 10 = 0");
+        System.out.println("Solving equation x^2 - 4x + 4 = 0");
         QuadraticEquationSolve.solve(1, -4, 4);
 
     }
