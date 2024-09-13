7946cc2de7cad21277512be10c007bd18f434799
S.Saha <soumabhasaha1509+github@gmail.com>
2024-09-13 22:44:05 +0530

    small corrections

--- Week1/OddEven.java
+++ Week1/OddEven.java
@@ -2,7 +2,7 @@
 public class OddEven {
 
     public static Boolean isOdd(Integer number) {
-        return (number % 2 == 0) ? (false) : (true);
+        return (number % 2 == 1);
     }
 
     public static Boolean isEven(Integer number) {
@@ -10,7 +10,7 @@ public class OddEven {
     }
 
     public static void main(String[] args) {
-        System.out.println("number 8 is odd ,true or false?\nAns)\t" + OddEven.isOdd(8));
-        System.out.println("number 9 is odd ,true or false?\nAns)\t" + OddEven.isOdd(9));
+        System.out.println("Number 8 is odd ,true or false?\nAns)\t" + OddEven.isOdd(8));
+        System.out.println("Number 9 is odd ,true or false?\nAns)\t" + OddEven.isOdd(9));
     }
 }
